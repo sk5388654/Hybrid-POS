66ea58aaa5a9c3af232036a56a82da88bc242786
MM-HO <sk5388654@gmail.com>
2025-11-10 14:34:10 +0500

    PWA implemented

--- vite.config.ts
+++ vite.config.ts
@@ -4,34 +4,21 @@ import vue from '@vitejs/plugin-vue'
 import copy from 'rollup-plugin-copy'
 import { defineConfig } from 'vite'
 import svg from 'vite-plugin-svgo'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Vue-app/', // <--- add this for GitHub Pages deployment
+  base: '/Vue-app/', // For GitHub Pages deployment
   build: {
     rollupOptions: {
       output: {
         manualChunks: (id) => {
           console.log('id: ', id)
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-
-          if (id.endsWith('.svg')) {
-            return path.basename(id)
-          }
-
-          if (id.includes('module-a')) {
-            return 'module-a'
-          }
-
-          if (id.includes('module-b')) {
-            return 'module-b'
-          }
-
-          if (id.includes('common')) {
-            return 'common'
-          }
+          if (id.includes('node_modules')) return 'vendor'
+          if (id.endsWith('.svg')) return path.basename(id)
+          if (id.includes('module-a')) return 'module-a'
+          if (id.includes('module-b')) return 'module-b'
+          if (id.includes('common')) return 'common'
         }
       },
       plugins: [
@@ -53,7 +40,39 @@ export default defineConfig({
         }
       }
     }),
-    svg()
+    svg(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.svg',
+        'robots.txt',
+        'apple-touch-icon.png',
+        'assets/manifest/android-chrome-192x192.png',
+        'assets/manifest/android-chrome-512x512.png'
+      ],
+      manifest: {
+        name: 'Vue Todo App',
+        short_name: 'TodoApp',
+        description: 'A simple offline-capable Todo App',
+        theme_color: '#4F46E5',
+        background_color: '#FFFFFF',
+        display: 'standalone',
+        scope: '/Vue-app/',
+        start_url: '/Vue-app/',
+        icons: [
+          {
+            src: 'assets/manifest/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'assets/manifest/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
